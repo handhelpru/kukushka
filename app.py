ae8fd780021c8f85bb789efa1692d9ca9acc98ad
e.chernousov <e.chernousov@aviconsult.ru>
2022-02-12 14:28:28 +0300

    Added backend scratch

---
+++ app.py
@@ -0,0 +1,51 @@
+import sys
+import traceback
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from datetime import datetime
+from loguru import logger
+
+import model_adapter
+
+
+logger.remove()  # Removes default handler
+logger.add(f"app.log",
+           format="<white>{time:YYYY-M-D HH:mm:ss:SS}</white> "
+           "<green>{module}:{function}:{line}</green> "
+           "<level>{message}</level>")
+logger.add(
+    sys.stderr, colorize=True, backtrace=True, diagnose=True,
+    format="<white>{time:YYYY-M-D HH:mm:ss:SS}</white> "
+           "<green>{module}:{function}:{line}</green> "
+           "<level>{message}</level>")
+
+
+class RequestModel(BaseModel):
+    # TODO: Add features
+    pass
+
+
+app = FastAPI()
+
+fe = model_adapter.FeatureExtractor()
+sklearn_adapter = model_adapter.SklearnAdapter(
+    model_fpath="model/model.pkl",
+    label_encoder_path="model/type_label_encoder.pkl"
+    )
+xgboost_adapter = model_adapter.XgBoostAdapter()
+
+
+@app.post("/predict")
+def predict(wav_file_request: RequestModel, request: Request):
+    ip = request.client.host  # Change to 'X-forwarded-from" if using NGINX
+    logger.info(f"Request from {ip}")
+
+    try:
+        logger.debug("Request file format: {}".format(wav_file_request.file_format))
+        now = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+        features = fe.extract_features(fpath=fpath)
+        return {"response": xgboost_adapter.predict(features)}
+
+    except BaseException as e:
+        logger.opt(exception=True).debug("Exception: ".format(e))
+        return {"Exception: ": traceback.print_exc()}
---
+++ model_adapter.py
@@ -0,0 +1,114 @@
+import sys
+import numpy as np
+import random
+from loguru import logger
+from joblib import load
+
+
+class FeatureExtractor:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def extract_features(request_data):
+        # feature_dict['filename'] = fpath
+        # feature_dict['duration'] = len(wf.waveform) / wf.sample_rate
+        logger.info(f"request_data: {request_data}")
+        features = np.array(list(request_data.values())).reshape(1, -1)
+        return features
+
+
+class SklearnAdapter:
+    models_fpath = ''
+
+    def __init__(self, **kwargs):
+        logger.info(f"Loading SklearnAdapter with kwargs: {kwargs}")
+        self.model = None
+
+    @staticmethod
+    def load_model(model_fpath: str):
+        return load(model_fpath)
+
+    @staticmethod
+    def test_predict(features):
+        logger.debug(f"Features size: {type(features)}, size: {sys.getsizeof(features)}")
+        prediction = {
+            "prediction": random.sample([0, 1], 1)[0],
+            "confidence": random.uniform(0, 1)
+        }
+        return prediction
+
+    @staticmethod
+    def predict(features):
+        model = SklearnAdapter.load_model(SklearnAdapter.model_fpath)
+        logger.debug(f"Features size: {type(features)}, size: {sys.getsizeof(features)}")
+        prediction = {
+            "prediction": model.predict(features),
+            "confidence": model.predict_proba(features)
+        }
+        return prediction
+
+
+class XgBoostAdapter:
+    model_fpath = ''
+    label_map = {
+        0: "",
+        1: ""
+    }
+
+    def __init__(self, **kwargs):
+        logger.info(f"Loading SklearnAdapter with kwargs: {kwargs}")
+        self.model = self.load_model(self.model_fpath)
+
+    @staticmethod
+    def load_model(model_fpath: str):
+        return load(model_fpath)
+
+    @staticmethod
+    def test_predict(features):
+        logger.debug(f"Features size: {type(features)}, size: {sys.getsizeof(features)}")
+        prediction = {
+            "prediction": random.sample([0, 1], 1)[0],
+            "confidence": random.uniform(0, 1)
+        }
+        return prediction
+
+    def predict_single(self, features):
+        logger.debug(f"Features size: {type(features)}, size: {sys.getsizeof(features)}")
+        prediction = self.model.predict(features)[0]
+        prediction_proba = self.model.predict_proba(features)[0]
+        logger.debug(f"Prediction: {prediction}")
+        logger.debug(f"prediction_proba: {prediction_proba}")
+        prediction = {
+            "prediction": self.label_map.get(prediction),
+            "confidence": str(max(prediction_proba)),
+        }
+        return prediction
+
+    def predict_multi(self, features):
+        logger.debug(f"Features size: {type(features)}, size: {sys.getsizeof(features)}")
+        prediction = self.model.predict(features)
+        prediction_proba = self.model.predict_proba(features)
+        logger.debug(f"Prediction: {prediction}")
+        logger.debug(f"prediction_proba: {prediction_proba}")
+        result = []
+        for p, pp in zip(prediction, prediction_proba):
+            prediction = {
+                "prediction": self.label_map.get(p),
+                "confidence": str(max(pp)),
+            }
+            result.append(prediction)
+
+        return result
+
+
+if __name__ == "__main__":
+    test_features = ""
+    features = FeatureExtractor().extract_features(test_features)
+
+    sklearn_adapter = SklearnAdapter(
+        model_fpath="model/model.pkl",
+        label_encoder_path="model/type_label_encoder.pkl"
+    )
+
+    logger.debug(sklearn_adapter.test_predict(features=features))
